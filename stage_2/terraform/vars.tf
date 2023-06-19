fa67c36d23fb3820edd8017c7cbc083ade75e6a0
marthamwangi09 <marthamwangi09@gmail.com>
2023-06-19 15:52:56 +0300

    Added the vars.tf file for define the variables

---
+++ stage_2/terraform/vars.tf
@@ -0,0 +1,14 @@
+# Creating a Variable for access_key
+variable "access_key" {
+  type = string
+}
+
+# Creating a Variable for secret_key
+variable "secret_key" {
+  type = string
+}
+
+# Creating a Variable for public_key
+variable "public_key" {
+  type = string
+}
